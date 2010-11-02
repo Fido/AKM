c1709b8b6422c44b5721436de8c4d940251ee589
Fido <sbfido@gmail.com>
2010-11-02 11:57:27 -0500

    Esqueleto inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AKM::Application.config.secret_token = '68cdbd94b13d6863aa90b8660a1cb535971298dfc4d33a85a9d27d268ab5daa8ff32519bbdcc16314d7c766be787e0682ec19be886d2959ad543f71957b572af'
